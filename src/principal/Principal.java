fac44fa52b23eb47a6a06ec9c32c846bebb0feb1
Elvis Aguilar <elat122e@gmail.com>
2021-06-18 13:43:57 -0600

    prueva

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,7 +1,7 @@
 package principal;
 
 import tablero.Tablero;
-
+//clase principal
 public class Principal {
     public static void main(String[] args){
         System.out.println("Bienvenido al Juego de Damas !");
